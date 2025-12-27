8d41fbf1de329fb1e9d2ff5c1000bc341ece6961
SHRIYANSH7711 <shriyanshnishad15711@gmail.com>
2025-12-27 15:04:53 +0530

    Few adjustments

--- Backend/add-amul.js
+++ Backend/add-amul.js
@@ -1,3 +1,4 @@
+// backend/add-amul.js
 require('dotenv').config();
 const pool = require('./db');
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-// server.js - COMPLETE VERSION
+// server.js
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
